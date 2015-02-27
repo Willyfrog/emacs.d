ed6dc2715a475d0a4dd1a2c178d191844d1a3ec7
Guillermo Vaya <guillermo.vaya@gigas.com>
2015-02-28 00:00:04 +0100

    font + ediff same frame

--- init.el
+++ init.el
@@ -208,7 +208,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Meslo LG S DZ" :foundry "bitstream" :slant normal :weight normal :height 128 :width normal)))))
 
 
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
@@ -324,6 +324,7 @@
    (quote
     ("208ace27fe9ce44edc36ed0f1b6149d389b6276827ad309b54d93a529d4cfb33" "df817d33ddab9ef51a69b05bcf464a9c8719dde1e5e669bb3c6719884e7ae34e" "d401b0920023533c5de1553d27e9d4669ae088f771cf2736108870f783af0cc0" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "cdf488af2fbc0735c3eeff42e77bc62cb14bd869a89c6a27a854e2c4a50c9ad2" "9bae7be09c7eba31130778f79b25ab5dc0fcf2af30588a7400343d99da3186e4" "10de032bf7ffb90f4d3ee9888c0dd1aa65549c00e5a6e22b5d606b0bbb1354ce" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "9b4f4a04c1770e7062bca28ab1a82f58c0ee18c4be74a98a85502fa7acf5bc89" "e80a0a5e1b304eb92c58d0398464cd30ccbc3622425b6ff01eea80e44ea5130e" "427234e4b45350b4159575f1ac72860c32dce79bb57a29a196b9cfb9dd3554d9" "5dfacaf380068d9ed06e0872a066a305ab6a1217f25c3457b640e76c98ae20e6" "99aae8e9489f7117284238c1cb0a1136147161e4c007c579bf28418603d96a5c" default)))
  '(display-battery-mode t)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(fringe-mode 4 nil (fringe))
  '(main-line-separator-style (quote chamfer))
  '(menu-bar-mode nil)
@@ -333,7 +334,9 @@
     ((virtualenv-default-directory . "~/Proyectos/hylink")
      (virtualenv-workon . "hylink")
      (virtualenv-default-directory . "~/Proyectos/gigas_api")
-     (virtualenv-workon . "api")))))
+     (virtualenv-workon . "api"))))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 
 ;; theme is the last thing to load, so if something breaks in the config,
 ;; everything will be white telling me that something went wrong
